62dc4d50590c509725bcf75033f0d6f6070e7948
Henrik Lissner <henrik@lissner.net>
2014-09-25 02:31:16 -0400

    Add yml spec stubs

--- spec/yml_spec.rb
+++ spec/yml_spec.rb
@@ -1,1 +1,17 @@
-describe "Yaml IO"
+describe "Yaml IO" do
+
+  describe ".read" do
+    it "raises an error when given nil"
+    it "raises an error when file doesn't exist"
+    it "converts a yaml list into an array"
+    it "converts a yaml dictionary into a dictionary"
+  end
+
+  describe ".write" do
+    it "raises an error when given nil"
+    it "raises an error when given a string"
+    it "converts a list to a yaml file"
+    it "converts a dictionary to a yaml file"
+  end
+
+end
