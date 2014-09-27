288357f58683d9a670462464740dbdcdb730d72f
Henrik Lissner <henrik@lissner.net>
2014-09-27 05:25:20 -0400

    Implement command stubs and basic CLI

--- lib/secgen.rb
+++ lib/secgen.rb
@@ -1,30 +1,18 @@
-# Security Simulator
-#
-# $Id$
-#
-# $Revision$
-#
-# This program allows you to use a large amount of virtual machines and install vulnerable software to create a learning environment.
-#
-# By: Lewis Ardern (Leeds Metropolitan University)
+$:.unshift __dir__
+
+require 'rubygems'
+require 'bundler/setup'
 
 require 'mercenary'
-
-require 'secgen/node'
-require 'secgen/nodeset'
-require 'secgen/puppet_manager'
-require 'secgen/resource_manager'
-require 'secgen/template'
-require 'secgen/controller'
-require 'secgen/cli'
-
-# require 'secgen/system'
-# require 'secgen/filecreator'
-# require 'secgen/systemreader'
-# require 'secgen/vagrant'
+# require 'liquid'
 
 module Secgen
-  VERSION="0.0.1"
+  VERSION="0.0.2"
+
+  autoload :ResourceManager, 'secgen/resource_manager'
+  autoload :Template,        'secgen/template'
+  autoload :Node,            'secgen/node'
+  autoload :NodeSet,         'secgen/nodeset'
 
   # Sets the Secgen configuration options.
   #
@@ -39,65 +27,7 @@ module Secgen
   def configure
     block_given? ? yield(Config) : Config
   end
-
-  def dispatch(*argv)
-    # forward commands to Secgen::CLI
-  end
 end
 
-# TODO: Rewrite with mercenary
-# TODO: Implement Secgen::CLI
-
-# def usage
-#   puts 'Usage:
-
-#    run - creates virtual machines e.g run 10
-
-#    kill - destoys current session
-
-#    ssh - creates a ssh session for specifiec box e.g ssh box1
-
-#    All options options are:
-#    --help -h: show
-#    --run -r: run
-# '
-#   exit
-# end
-
-# def run
-# 	puts 'reading configuration file on how many virtual machines you want to create'
-
-# 	puts 'creating vagrant file'
-#   # uses nokogoiri to grab all the system information from boxes.xml
-#   systems = SystemReader.new(BOXES_XML).systems
-
-#    # create's vagrant file / report a starts the vagrant installation'
-#   create_files = FileCreator.new(systems)
-#   build_number = create_files.generate(systems)
-
-#   vagrant = VagrantController.new
-#   vagrant.vagrant_up(build_number)
-# end
-
-# def config
-# 	usage
-# end
-
-# opts = GetoptLong.new(
-#   [ '--help', '-h', GetoptLong::NO_ARGUMENT ],
-#   [ '--run', '-r', GetoptLong::NO_ARGUMENT ],
-#   [ '--config', '-c', GetoptLong::NO_ARGUMENT ]
-# )
-
-# opts.each do |opt, arg|
-#   case opt
-#     when '--help'
-#       usage
-#     when '--run'
-#     	run
-#     when '--config'
-#     	#do a box count increment to next one
-#     	#create template config file!
-#     	config
-#   end
-# end
+require 'secgen/command'
+Dir["#{__dir__}/secgen/commands/*.rb"].each { |p| require p }
