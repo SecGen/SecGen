30822149d86793eebc21007968d3c0b874f1dd15
Henrik Lissner <henrik@lissner.net>
2015-03-20 03:45:35 -0400

    Write cli_spec stubs

--- spec/secgen/cli_spec.rb
+++ spec/secgen/cli_spec.rb
@@ -1,21 +1,88 @@
 describe "CLI" do
+  describe "#new" do
+    it "generates ./Nodefile"
+    it "interpolates variables in ./Nodefile"
+    it "fails gracefully if CWD is already a node (or issue)"
+  end
 
-  describe "#new-base"
+  describe "#info" do
+    context "when CWD is a node" do
+      it "reports node information"
+    end
 
-  describe "#new"
+    context "when CWD is not a node" do
+      it "fails gracefully"
+    end
+  end
 
-  describe "#add"
+  describe "#group" do
+    context "when destination is invalid" do
+      it "fails gracefully"
+    end
 
-  describe "#rm"
+    context "when specified nodes are invalid" do
+      it "fails gracefully"
+    end
 
-  describe "#destroy"
+    context "when specified nodes are valid" do
+      it "fails gracefully"
+      it "creates $dest/Nodesetfile"
+      it "creates $dest/$node/ folder for each Node"
+    end
+  end
 
-  describe "#edit"
+  describe "#update" do
+    it "accepts no args (using CWD)"
+    it "accepts node path"
+    it "accepts node name"
 
-  describe "#list"
+    context "when specified node is invalid" do
+      it "fails gracefully"
+    end
 
-  describe "#test"
+    context "when specified node is valid" do
+      it "checks for updates"
 
-  describe "#deploy"
+      context "updates are available" do
+        it "pulls from issue's repo"
+      end
 
+      context "updates are not available" do
+        it "fails gracefully"
+      end
+    end
+  end
+
+  describe "#deploy" do
+    context "when 404 or 500" do
+      it "fails gracefully"
+    end
+
+    context "it fails to build" do
+      it "fails gracefully"
+      it "cleans up after itself"
+    end
+  end
+
+  describe "#issue" do
+    describe "#new" do
+      it "generates ./Issuefile"
+      it "interpolates variables in ./Issuefile"
+      it "fails gracefully if CWD is already a issue (or node)"
+    end
+
+    describe "#verify" do
+      it "detects ruby syntax errors"
+      it "detects invalid dependencies"
+      it "detects template errors"
+    end
+
+    describe "#search" do
+      context "on 401, 404 or 500" do
+        it "fails gracefully"
+      end
+
+      it "prints a issue-name => url list to stdout"
+    end
+  end
 end
