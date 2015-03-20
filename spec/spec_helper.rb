3b9760d66b0d6353dbba43c9771fadb8f050d367
Henrik Lissner <henrik@lissner.net>
2015-03-20 03:31:08 -0400

    Update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'factory_girl'
 require 'rspec/its'
 
-RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
+RSpec.configure do |c|
+  c.include FactoryGirl::Syntax::Methods
+  c.tty = true
 end
