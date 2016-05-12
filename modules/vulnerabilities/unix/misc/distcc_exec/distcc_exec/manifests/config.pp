dbb78104e71595b38360665fd3b5eb39260ba6c8
aviio <connorwilson1@googlemail.com>
2016-05-12 18:13:56 +0100

    Fixes distcc vulnerability issue and removes environment warnings

---
+++ modules/vulnerabilities/unix/misc/distcc_exec/distcc_exec/manifests/config.pp
@@ -0,0 +1,10 @@
+class distcc_exec::config{
+  file { '/etc/default/distcc':
+    require => Package['distcc'],
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0777',
+    content  => template('distcc_exec/distcc.erb')
+  }
+}
