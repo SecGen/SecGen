c63369f81f169f38c5e253cc57b94c1c042bcf8f
Jjk422 <j.keughley5886@student.leedsbeckett.ac.uk>
2016-08-15 17:19:39 +0100

    Fix for the samba module, merge of legacy code into Cliffe/master left module broken. Removed all legacy and non samba related code. Tested and seems to be working.

--- modules/services/unix/file_share/samba/samba.pp
+++ modules/services/unix/file_share/samba/samba.pp
@@ -1,11 +1,1 @@
-class { 'samba':
-  require => Exec['update'],
-  puppi => true,
-  # monitor      => true,
-  # monitor_tool => [ 'nagios' , 'monit' , 'munin' ],
-  firewall      => true,
-  firewall_tool => 'iptables',
-  firewall_src  => '10.42.0.0/24',
-  firewall_dst  => $ipaddress_eth0,
-}
-
+class { 'samba': }
