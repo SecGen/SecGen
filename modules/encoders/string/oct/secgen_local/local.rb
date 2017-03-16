8d61097be54b5e64d18e541ec1438bfb7ad0398b
thomashaw <thomashaw@gmail.com>
2017-03-16 12:21:03 +0000

    encoders/string/hex: string to oct encoder - Encodes a string into each character's octal representation

--- modules/encoders/string/oct/secgen_local/local.rb
+++ modules/encoders/string/oct/secgen_local/local.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/ruby
 require_relative '../../../../../lib/objects/local_string_encoder.rb'
-class HexEncoder < StringEncoder
+class OctalEncoder < StringEncoder
   def initialize
     super
-    self.module_name = 'Hexadecimal Encoder'
+    self.module_name = 'Octal Encoder'
   end
 
   def encode(str)
     encoded_char_array = []
-    str.each_char { |char| encoded_char_array << char.ord.to_s(16) }
+    str.each_char { |char| encoded_char_array << char.ord.to_s(8) }
     encoded_char_array.join
   end
 end
 
-HexEncoder.new.run
+OctalEncoder.new.run
