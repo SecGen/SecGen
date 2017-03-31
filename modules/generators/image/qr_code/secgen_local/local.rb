04399470f23a491f8eff9ebfb8f542e8ca16a62d
thomashaw <thomashaw@gmail.com>
2017-03-31 19:21:36 +0100

    updated scenarios, metadata changes + updated qr code

--- modules/generators/image/qr_code/secgen_local/local.rb
+++ modules/generators/image/qr_code/secgen_local/local.rb
@@ -2,35 +2,35 @@
 require 'rqrcode'
 require_relative '../../../../../lib/objects/local_string_encoder.rb'
 class QRCodeGenerator < StringEncoder
-  attr_accessor :string_to_mask
+  attr_accessor :strings_to_leak
 
   def initialize
     super
     self.module_name = 'QR Code Generator'
-    self.string_to_mask = []
+    self.strings_to_leak = []
   end
 
   def encode_all
-    qr_code = RQRCode::QRCode.new(self.string_to_mask[0])
+    qr_code = RQRCode::QRCode.new(self.strings_to_leak[0])
     image = qr_code.as_png
     self.outputs <<  Base64.strict_encode64(image.to_blob)
   end
 
   def get_options_array
-    super + [['--string_to_mask', GetoptLong::REQUIRED_ARGUMENT]]
+    super + [['--strings_to_leak', GetoptLong::REQUIRED_ARGUMENT]]
   end
 
   def process_options(opt, arg)
     super
     case opt
-      when '--string_to_mask'
-        self.string_to_mask << arg;
+      when '--strings_to_leak'
+        self.strings_to_leak << arg;
     end
   end
 
 
   def encoding_print_string
-    'String to mask: ' + self.string_to_mask.first
+    'Strings_to_leak: ' + self.strings_to_leak.first
   end
 end
 
