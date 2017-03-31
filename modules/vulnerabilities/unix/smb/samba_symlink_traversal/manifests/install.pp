bf8d15e395edf0685f3dee6633e8a1892d000789
thomashaw <thomashaw@gmail.com>
2017-03-31 14:57:06 +0100

    added a flag drop in root directory to samba_symlink_traversal

--- modules/vulnerabilities/unix/smb/samba_symlink_traversal/manifests/install.pp
+++ modules/vulnerabilities/unix/smb/samba_symlink_traversal/manifests/install.pp
@@ -38,11 +38,20 @@ class samba_symlink_traversal::install {
     command => "/bin/sed -i \'/\\[global\\]/a allow insecure wide links = yes\' /etc/samba/smb.conf"
   }
 
-  ::secgen_functions::leak_files { 'samba_symlink_traversal-file-leak':
+  # Leak a flag/string to the samba share directory
+  ::secgen_functions::leak_files { 'samba_symlink_traversal-file-leak-1':
     storage_directory => $storage_directory,
-    leaked_filenames  => $leaked_filenames,
-    strings_to_leak   => $strings_to_leak,
+    leaked_filenames  => [$leaked_filenames[0]],
+    strings_to_leak   => [$strings_to_leak[0]],
     images_to_leak    => $images_to_leak,
+    leaked_from       => 'samba_symlink_traversal',
+  }
+
+  # Leak a flag/string to root directory
+  ::secgen_functions::leak_files { 'samba_symlink_traversal-file-leak-2':
+    storage_directory => '/',
+    leaked_filenames  => [$leaked_filenames[1]],
+    strings_to_leak   => [$strings_to_leak[1]],
     leaked_from       => 'samba_symlink_traversal',
   }
 }
