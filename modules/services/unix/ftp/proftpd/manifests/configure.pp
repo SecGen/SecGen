3d4c0fa98a661d0bda25635b025c679f4560ea9f
thomashaw <thomashaw@gmail.com>
2017-04-19 13:17:48 +0100

    Parameterised port - service/proftpd

--- modules/services/unix/ftp/proftpd/manifests/configure.pp
+++ modules/services/unix/ftp/proftpd/manifests/configure.pp
@@ -1,9 +1,11 @@
 class proftpd::configure {
+  $json_inputs = base64('decode', $::base64_inputs)
   file { '/etc/proftpd/proftpd.conf':
+    notify   => Service['proftpd'],
     ensure   => present,
     owner    => 'root',
     group    => 'root',
     mode     => '0644',
-    content  => template('proftpd/proftpd.erb')
+    content  => template('proftpd/proftpd.erb'),
   }
 }
