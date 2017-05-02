943a614db702ed169f60bd5fac19775a576b33fe
thomashaw <thomashaw@gmail.com>
2017-05-02 16:46:54 +0100

    Added hints to string encoders. Prepend leading 0's to decimal and octal encoders.

--- modules/encoders/string/oct/secgen_local/local.rb
+++ modules/encoders/string/oct/secgen_local/local.rb
@@ -8,7 +8,18 @@ class OctalEncoder < StringEncoder
 
   def encode(str)
     encoded_char_array = []
-    str.each_char { |char| encoded_char_array << char.ord.to_s(8) }
+    str.each_char { |char|
+      converted_char = char.ord.to_s(8)
+
+      # Pad with leading 0s
+      if converted_char.length == 1
+        converted_char = "00#{converted_char}"
+      elsif converted_char.length == 2
+        converted_char = "0#{converted_char}"
+      end
+
+      encoded_char_array << converted_char
+    }
     encoded_char_array.join
   end
 end
