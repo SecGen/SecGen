d791b15ad49b8017a77e41805fa27b45f826e4b6
thomashaw <thomashaw@gmail.com>
2017-08-08 16:17:35 +0100

    parameterised_website: now accepts blank organisation

--- modules/services/unix/http/parameterised_website/manifests/install.pp
+++ modules/services/unix/http/parameterised_website/manifests/install.pp
@@ -2,20 +2,30 @@ class parameterised_website::install {
   $json_inputs = base64('decode', $::base64_inputs)
   $secgen_parameters = parsejson($json_inputs)
 
-  # Parse out parameters
-  $business_name = $secgen_parameters['business_name'][0]
-  $business_motto = $secgen_parameters['business_motto'][0]
-  if $secgen_parameters['manager_profile'][0] != '' {
-    $manager_profile =parsejson($secgen_parameters['manager_profile'][0])
+  $organisation = $secgen_parameters['organisation'][0]
+
+  if $organisation and $organisation != '' {
+    $business_name = $organisation['business_name']
+    $business_motto = $organisation['business_motto']
+    $manager_profile = $organisation['manager']
+    $business_address = $organisation['business_address']
+    $office_telephone = $organisation['office_telephone']
+    $office_email = $organisation['office_email']
+    $industry = $organisation['industry'][0]
+    $product_name = $organisation['product_name']
+    $employees = $organisation['employees']
   } else {
+    $business_name = ''
+    $business_motto = ''
     $manager_profile = ''
+    $business_address = ''
+    $office_telephone = ''
+    $office_email = ''
+    $industry = ''
+    $product_name = ''
+    $employees = []
   }
-  $business_address = $secgen_parameters['business_address'][0]
-  $office_telephone = $secgen_parameters['office_telephone'][0]
-  $office_email = $secgen_parameters['office_email'][0]
-  $industry = $secgen_parameters['industry'][0]
-  $product_name = $secgen_parameters['product_name'][0]
-  $employees = $secgen_parameters['employees']
+
   $strings_to_leak = $secgen_parameters['strings_to_leak']
   $main_page_paragraph_content = $secgen_parameters['main_page_paragraph_content']
   $images_to_leak = $secgen_parameters['images_to_leak']
@@ -40,31 +50,39 @@ class parameterised_website::install {
     $index_template = 'parameterised_website/index.html.erb'
   }
 
+  file { $docroot:
+    ensure => directory,
+  }
+
   # Move boostrap css+js over
   file { "$docroot/css":
     ensure => directory,
     recurse => true,
     source => 'puppet:///modules/parameterised_website/css',
+    require => File[$docroot],
   }
   file { "$docroot/js":
     ensure => directory,
     recurse => true,
     source => 'puppet:///modules/parameterised_website/js',
+    require => File[$docroot],
   }
 
   # Apply default CSS template
   file { "$docroot/css/default.css":
-  ensure => file,
-  content => template('parameterised_website/default.css.erb')
+    ensure => file,
+    content => template('parameterised_website/default.css.erb'),
+    require => File["$docroot/css"],
   }
 
   # Apply index page template
   file { "$docroot/index.html":
     ensure  => file,
     content => template($index_template),
+    require => File[$docroot],
   }
 
-  if $manager_profile != '' or $business_address != '' or $employees[0] != '' {
+  if $organisation and $organisation != ''{
     # Apply contact page template
     file { "$docroot/contact.html":
       ensure  => file,
@@ -108,14 +126,16 @@ class parameterised_website::install {
     leaked_from => "parameterised_website",
   }
 
-  $additional_page_pairs = zip($additional_pages, $additional_page_filenames)
-  $additional_page_pairs.each |$additional_page_pair|{
-    $additional_page_contents = $additional_page_pair[0]
-    $additional_page_filename = $additional_page_pair[1]
+  if $additional_pages and $additional_page_filenames {
+    $additional_page_pairs = zip($additional_pages, $additional_page_filenames)
+    $additional_page_pairs.each |$additional_page_pair|{
+      $additional_page_contents = $additional_page_pair[0]
+      $additional_page_filename = $additional_page_pair[1]
 
-    file { "$docroot/$additional_page_filename":
-      ensure  => file,
-      content => template('parameterised_website/page.html.erb'),
+      file { "$docroot/$additional_page_filename":
+        ensure  => file,
+        content => template('parameterised_website/page.html.erb'),
+      }
     }
   }
 }
