8b2a92915e32d47f76a5ea63c9a73e6249834486
thomashaw <thomashaw@gmail.com>
2017-08-19 13:44:16 +0100

    vulnerabilities/vsftpd_234_backdoor: now uses organisations

--- modules/vulnerabilities/unix/ftp/vsftpd_234_backdoor/manifests/config.pp
+++ modules/vulnerabilities/unix/ftp/vsftpd_234_backdoor/manifests/config.pp
@@ -1,6 +1,14 @@
 class vsftpd_234_backdoor::config {
 
   $json_inputs = base64('decode', $::base64_inputs)
+  $secgen_parameters = parsejson($json_inputs)
+  $raw_org = $secgen_parameters['organisation']
+  if $raw_org and $raw_org[0] and $raw_org[0] != '' {
+    $organisation = parsejson($raw_org[0])
+  } else {
+    $organisation = ''
+  }
+
   # Config files + manuals
   file { ['/usr/local/man/man5/vsftpd.conf.5']:
     require => File['/usr/local/src/vsftpd-2.3.4/Makefile'],
