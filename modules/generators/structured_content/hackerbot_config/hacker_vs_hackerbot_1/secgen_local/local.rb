f9c07643976c5a5cdf7354bcd091408e22b69fb6
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2017-11-02 17:34:55 +0000

    HvHB1 updates

--- modules/generators/structured_content/hackerbot_config/hacker_vs_hackerbot_1/secgen_local/local.rb
+++ modules/generators/structured_content/hackerbot_config/hacker_vs_hackerbot_1/secgen_local/local.rb
@@ -4,7 +4,7 @@ require_relative '../../../../../../lib/objects/local_hackerbot_config_generator
 class IDS < HackerbotConfigGenerator
 
   attr_accessor :backup_server_ip
-  attr_accessor :ids_server_ip
+  attr_accessor :desktop_ip
   attr_accessor :hackerbot_server_ip
 
   def initialize
@@ -18,13 +18,13 @@ class IDS < HackerbotConfigGenerator
     self.html_template_path = "#{self.local_dir}/templates/labsheet.html.erb"
 
     self.backup_server_ip = []
-    self.ids_server_ip = []
+    self.desktop_ip = []
     self.hackerbot_server_ip = []
   end
 
   def get_options_array
     super + [['--backup_server_ip', GetoptLong::REQUIRED_ARGUMENT],
-             ['--ids_server_ip', GetoptLong::REQUIRED_ARGUMENT],
+             ['--desktop_ip', GetoptLong::REQUIRED_ARGUMENT],
              ['--hackerbot_server_ip', GetoptLong::REQUIRED_ARGUMENT]]
   end
 
@@ -33,10 +33,10 @@ class IDS < HackerbotConfigGenerator
     case opt
       when '--backup_server_ip'
         self.backup_server_ip << arg;
-      when '--ids_server_ip'
-        self.ids_server_ip << arg;
+      when '--desktop_ip'
+        self.desktop_ip << arg;
       when '--hackerbot_server_ip'
-        self.ids_server_ip << arg;
+        self.desktop_ip << arg;
     end
   end
 
