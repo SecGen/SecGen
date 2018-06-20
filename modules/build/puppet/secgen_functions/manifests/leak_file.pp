aafaa3d6480afdd02c3b6930350c9f0bd198ab62
thomashaw <thomashaw@gmail.com>
2018-06-20 19:14:46 +0100

    fixed puppet resource naming conflict

--- modules/build/puppet/secgen_functions/manifests/leak_file.pp
+++ modules/build/puppet/secgen_functions/manifests/leak_file.pp
@@ -3,7 +3,7 @@ define secgen_functions::leak_file($leaked_filename, $storage_directory, $string
     $path_to_leak = "$storage_directory/$leaked_filename"
 
     # create the directory tree, incase the file name has extra layers of directories
-    exec { "$leaked_from-$path_to_leak":
+    exec { "$leaked_from-$path_to_leak-mkdir":
       path    => ['/bin', '/usr/bin', '/usr/local/bin', '/sbin', '/usr/sbin'],
       command => "mkdir -p `dirname $path_to_leak`;chown $owner. `dirname $path_to_leak`",
       provider => shell,
@@ -12,7 +12,7 @@ define secgen_functions::leak_file($leaked_filename, $storage_directory, $string
     # If the file already exists append to it, otherwise create it.
     if (defined(File[$path_to_leak])){
       notice("File with that name already defined, appending leaked strings instead...")
-      exec { "$leaked_from-$path_to_leak":
+      exec { "$leaked_from-$path_to_leak-append":
         path    => ['/bin', '/usr/bin', '/usr/local/bin', '/sbin', '/usr/sbin'],
         command => "echo \"\n------\n$strings_to_leak\" >> $path_to_leak",
       }
