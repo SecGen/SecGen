7b75aa2b852b69a0bdb2e646519aa10807bdb596
thomashaw <thomashaw@gmail.com>
2018-08-10 16:07:28 +0100

    Task 18.1 - echo string challenge working over the network

--- modules/generators/challenges/misc/programming/echo_string/secgen_local/challenge.rb
+++ modules/generators/challenges/misc/programming/echo_string/secgen_local/challenge.rb
@@ -1,17 +1,19 @@
 require 'securerandom'
 require 'timeout'
 
+$stdout.sync = true  # Disable stdout caching (for challenges that can be run over the network)
 puts 'Prepare yourself. You need to work quickly for this challenge.'
+
 sleep 2
 puts 'Echo the string back to me before the script times out. Try writing a script of your own to make it easier!'
 sleep 2
 puts 'Get ready, here we go...'
 sleep 2
-
 # Generate random string
 string = [SecureRandom.base64(rand(20..40)), SecureRandom.hex(rand(20..40))].sample
 
 puts string
+STDOUT.flush
 
 begin
   Timeout.timeout 0.25 do
