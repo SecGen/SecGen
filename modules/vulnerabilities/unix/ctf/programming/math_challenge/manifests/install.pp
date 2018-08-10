b9f56bbe1015d0451a26ca91e7d370e12ffaa4ee
thomashaw <thomashaw@gmail.com>
2018-08-10 15:46:59 +0100

    Difficulty cont.

--- modules/vulnerabilities/unix/ctf/programming/math_challenge/manifests/install.pp
+++ modules/vulnerabilities/unix/ctf/programming/math_challenge/manifests/install.pp
@@ -5,7 +5,7 @@ class math_challenge::install {
   ::secgen_functions::install_setgid_script { $challenge_name:
     source_module_name => $module_name,
     challenge_name     => $challenge_name,
-    script_name        => "$challenge_name  .rb",
+    script_name        => "$challenge_name.rb",
     script_data        => $secgen_params['script_data'],
     group              => $secgen_params['group'],
     account            => $secgen_params['account'],
