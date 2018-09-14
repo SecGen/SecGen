63d2c54f1e1a96b849fc9f9558fd118d09c0ca31
ts <thomashaw@localhost.localdomain>
2018-09-14 10:57:27 +0100

    Auditbeat pushing correctly with file integrity module

--- modules/utilities/unix/logging/auditbeat/auditbeat.pp
+++ modules/utilities/unix/logging/auditbeat/auditbeat.pp
@@ -10,15 +10,37 @@ class { 'auditbeat':
       'paths'   => ['/bin', '/usr/bin', '/sbin', '/usr/sbin', '/etc'],
     },
     # {
-    #   'module'  => 'auditd',
-    #   'enabled' => true,
-    #   '' => [''],  TODO: this needs correctly configuring. see https://www.elastic.co/guide/en/beats/auditbeat/current/auditbeat-module-auditd.html
+      # 'module'  => 'auditd',
+      # 'enabled' => true,
+      # 'audit_rules' => '-a always,exit -F arch=b64 -S all -F key=64bit-abi',
+       # TODO: this needs correctly configuring. see https://www.elastic.co/guide/en/beats/auditbeat/current/auditbeat-module-auditd.html
     # },
   ],
   outputs => {
     'logstash' => {
-      'hosts' => ["http://$logstash_ip:$logstash_port"],
-      'index' => 'auditbeat-%{+YYYY.MM.dd}',
+      'hosts' => ["$logstash_ip:$logstash_port"],
     },
   },
 }
+
+
+#
+# class { 'auditbeat':
+#   modules => [
+#     {
+#       'module'  => 'file_integrity',
+#       'enabled' => true,
+#       'paths'   => ['/bin', '/usr/bin', '/sbin', '/usr/sbin', '/etc'],
+#     },
+#     {
+#       'module'      => 'auditd',
+#       'enabled'     => true,
+#     },
+#   ],
+#   outputs => {
+#     'elasticsearch' => {
+#       'hosts' => ['http://localhost:9200'],
+#       'index' => 'auditbeat-%{+YYYY.MM.dd}',
+#     },
+#   }
+# }
