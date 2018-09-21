0ca88194b17c8d8c2996e1aeac172300a626d1ba
ts <thomashaw@localhost.localdomain>
2018-09-21 16:50:29 +0100

    more stable tomcat url#

--- modules/services/unix/http/tomcat/tomcat.pp
+++ modules/services/unix/http/tomcat/tomcat.pp
@@ -1,5 +1,5 @@
 tomcat::install { '/opt/tomcat':
-  source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-7/v7.0.90/bin/apache-tomcat-7.0.90.tar.gz',
+  source_url => 'https://archive.apache.org/dist/tomcat/tomcat-7/v7.0.91/bin/apache-tomcat-7.0.91.tar.gz',
 }
 tomcat::instance { 'default':
   catalina_home => '/opt/tomcat',
