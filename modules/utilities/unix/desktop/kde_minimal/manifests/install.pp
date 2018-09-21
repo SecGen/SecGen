daafd6637b9e67067daf45c418e016c577ad305c
ts <thomashaw@localhost.localdomain>
2018-09-21 15:21:15 +0100

    kde module updated for debian 9 stretch (todo: test me!)

--- modules/utilities/unix/desktop/kde_minimal/manifests/install.pp
+++ modules/utilities/unix/desktop/kde_minimal/manifests/install.pp
@@ -1,11 +1,9 @@
 class kde_minimal::install{
-  # case $operatingsystem {
-    # 'Debian': {
-    #   package { ['kde-plasma-desktop', 'kate', 'ksnapshot', 'qtcurve', 'kdesudo']:
-    #     ensure => 'installed',
-    #   }
-    #   # TODO: reboot if kde-plasma-desktop just installed ~>
-    #
-    # }
-  # }
+  case $operatingsystem {
+    'Debian': {
+      package { ['kde-plasma-desktop', 'kate', 'ksnapshot', 'qtcurve', 'kdesudo']:
+        ensure => 'installed',
+      }
+    }
+  }
 }
