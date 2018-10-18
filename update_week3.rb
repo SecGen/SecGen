8c364e20fd3be8a230fa856c826bd7c1db91b0ec
ts <thomashaw@localhost.localdomain>
2018-10-18 12:10:59 +0100

    fix

--- update_week3.rb
+++ update_week3.rb
@@ -1,4 +1,9 @@
+vm_name = "p-2-4-11-iMQL-3-backups-and-recovery-backup-server"
 new_ip_addr = "10.170.92.3"
 old_ip_addr = "172.22.6.255"
 
+puts "running command against #{vm_name} updating #{old_ip_addr} to #{new_ip_addr}"
 output = `echo "auto lo\niface lo inet loopback\n\nauto eth0\niface eth0 inet static\n\taddress #{new_ip_addr}" | ssh vagrant@#{old_ip_addr} "sudo sh -c 'cat >/etc/network/interfaces'"`
+
+puts 'command output: '
+puts output
