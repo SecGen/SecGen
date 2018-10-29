f7d83a94c1449ce7f6101b19a529c25c12d0e3df
ts <thomashaw@localhost.localdomain>
2018-10-29 17:47:26 +0000

    WiP: stripped out web stuff from installer.sh + automatically run it

--- modules/utilities/unix/ctf/metactf/files/repository/install.sh
+++ modules/utilities/unix/ctf/metactf/files/repository/install.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-# Installs on Google Cloud Platform's Compute Engine for Ubuntu 18.04 LTS
-# Make sure script is called with DNS name desired
-if [ $# -ne 1 ]; then
-    echo "Usage: sudo install.sh <DNS_Name>"
-fi
-
-# Name the service based on first part of DNS name
-SITE=`echo $1 | sed -e 's/\..*//'`
-
 # Install all required system packages
 apt update
 apt install -y python-pip python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools virtualenv nginx python-certbot-nginx libc6-dev-i386 g++-multilib zsh openssl upx bc
@@ -20,32 +11,4 @@ virtualenv -p python3 env
 source env/bin/activate
 pip install --upgrade -r requirements.txt
 
-# Set up systemd service for site
-sed s+PROJECT_USER+$SUDO_USER+ etc/systemd.template | sed s+PROJECT_DIR+$PWD+ > /etc/systemd/system/$SITE.service
-
-# Configure nginx for site
-sed s+PROJECT_HOST+$1+ etc/nginx.template | sed s+PROJECT_DIR+$PWD+ > /etc/nginx/sites-available/$SITE
-ln -s /etc/nginx/sites-available/$SITE /etc/nginx/sites-enabled
-
-# Change ownership to regular user, but make group www-data so that
-#   nginx can access.  Note: unix socket must be created by nginx in
-#   current directory so make www-data own the top level directory
-chown -R $SUDO_USER $PWD
-chgrp -R www-data $PWD
-chmod g+wX -R $PWD
-chown www-data $PWD
-
-# For metactf, regular user creates solved directories that the server
-#   must be able to write to.  Add user to www-data group.
-usermod -g www-data $SUDO_USER
-sed -i -e '/^UMASK/ s/022/002/' /etc/login.defs
-
-# Restart all services
-systemctl start $SITE
-systemctl enable $SITE
-systemctl restart nginx
-
-certbot --nginx -d $1 -n -m wuchang@pdx.edu --agree-tos
-
-#   must be able to write to.  Add user to www-data group.
-echo "Installation complete.  Please logout and log back in for group and umask changes to take effect"
+echo "Installation complete."
--- modules/utilities/unix/ctf/metactf/manifests/install.pp
+++ modules/utilities/unix/ctf/metactf/manifests/install.pp
@@ -1,9 +1,20 @@
 class metactf::install {
+  $install_dir = '/opt/metactf'
 
-  file { '/usr/lib/metactf':
+  Exec { path => ['/bin', '/usr/bin', '/usr/local/bin', '/sbin', '/usr/sbin'] }
+
+  file { $install_dir:
     ensure => directory,
     recurse => true,
     source => 'puppet:///modules/metactf/repository',
+  }
+
+  exec { 'set install.sh mode':
+    command => "chmod +x $install_dir/install.sh",
+  }
+
+  exec { 'install metactf dependencies':
+    command => "/bin/bash $install_dir/install.sh"
   }
 
 }
