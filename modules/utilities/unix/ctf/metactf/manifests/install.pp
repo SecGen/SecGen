700ca0d4379cf0785ce0b8f6788c454cf094a268
ts <thomashaw@localhost.localdomain>
2018-10-29 17:59:53 +0000

    WiP: stripped out web stuff from installer.sh + automatically run it

--- modules/utilities/unix/ctf/metactf/manifests/install.pp
+++ modules/utilities/unix/ctf/metactf/manifests/install.pp
@@ -17,4 +17,14 @@ class metactf::install {
     command => "/bin/bash $install_dir/install.sh"
   }
 
+  # Determine how best to generate individual challenges at scenario level.
+
+  # For now just build all of the binaries.
+
+  # Modify the 'users file' to use accounts{} ? Do we even bother? It appears to only be used on the webapp anyway.
+  # The filename is irrelevant.
+
+  # Move the challenges based on account name.
+
+
 }
