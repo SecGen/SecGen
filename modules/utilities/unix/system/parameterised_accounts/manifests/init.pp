3d3222bba2a3697255bd912e4da96fba65cc6627
ts <thomashaw@localhost.localdomain>
2018-10-30 15:13:08 +0000

    parameterised accounts removed default input (it was previously unused in any scenario + needed to include the module without generating an empty unused account)

--- modules/utilities/unix/system/parameterised_accounts/manifests/init.pp
+++ modules/utilities/unix/system/parameterised_accounts/manifests/init.pp
@@ -2,7 +2,8 @@ class parameterised_accounts::init {
   $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
 
   $accounts = $secgen_parameters['accounts']
-  $accounts.each |$raw_account| {
+  if $accounts {
+    $accounts.each |$raw_account| {
       $account = parsejson($raw_account)
       $username = $account['username']
       parameterised_accounts::account { "parameterised_$username":
@@ -13,4 +14,5 @@ class parameterised_accounts::init {
         leaked_filenames => $account['leaked_filenames']
       }
     }
+  }
 }
