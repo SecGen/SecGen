52094048399f7e4d5ba64668fb73e9e0f38afd43
ts <thomashaw@localhost.localdomain>
2018-10-31 14:54:17 +0000

    MetaCTF: src_malware and src_csp challenges now building path correctly using relative path in challenge list + random metactf challenge generator

--- modules/utilities/unix/ctf/metactf/manifests/configure.pp
+++ modules/utilities/unix/ctf/metactf/manifests/configure.pp
@@ -4,6 +4,7 @@ class metactf::configure {
   $challenge_list = $secgen_params['challenge_list']
   $flags = $secgen_params['flags']
   $groups = $secgen_params['groups']
+  $include_scaffolding = str2bool($secgen_params['include_scaffolding'][0])
 
   $raw_account = $secgen_params['account'][0]
   $account = parsejson($raw_account)
@@ -23,15 +24,23 @@ class metactf::configure {
   $challenge_pairs = zip($challenge_list, $flags)
 
   $challenge_pairs.each |$counter, $challenge_pair| {
-    $challenge_path = $challenge_pair[0]
     $flag = $challenge_pair[1]
-    $split_challenge = split($challenge_path, '/')
-    $metactf_challenge_dir = $split_challenge[0]
-    $metactf_challenge_type = split($metactf_challenge_dir, '_')[1]
-    $challenge_name = $split_challenge[1]
-    $group = $groups[$counter]
+    $challenge_path = $challenge_pair[0]
 
-    $binary_path = "$install_dir/$metactf_challenge_dir/obj/secgen/$metactf_challenge_type/$challenge_name"
+    $split_challenge = split($challenge_path, '/')
+    $metactf_challenge_category = $split_challenge[0]
+
+    if $metactf_challenge_category == 'src_angr'{
+      $metactf_challenge_type = split($metactf_challenge_category, '_')[1]
+      $challenge_name = $split_challenge[1]
+      $binary_path = "$install_dir/$metactf_challenge_category/obj/secgen/$metactf_challenge_type/$challenge_name"
+    } else {
+      $challenge_outer_dir = $split_challenge[1]
+      $challenge_name = $split_challenge[2]
+      $binary_path = "$install_dir/$metactf_challenge_category/$challenge_outer_dir/$challenge_name/obj/secgen/$challenge_name"
+    }
+
+    $group = $groups[$counter]
 
     ::secgen_functions::install_setgid_binary { "metactf_$challenge_name":
       source_module_name => $module_name,
@@ -43,6 +52,11 @@ class metactf::configure {
       binary_path        => $binary_path,
       storage_dir        => $storage_dir,
       strings_to_leak    => $secgen_params['strings_to_leak'],
+    }
+
+    # TODO
+    if $include_scaffolding {
+      # Add scaffolding file
     }
   }
 
