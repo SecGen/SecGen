a5d2a1cf6523081ee5c4015a1e69d599fae54217
ts <thomashaw@localhost.localdomain>
2018-11-07 17:20:05 +0000

    Adding pax-utils to reversing_tools (contains dumpelf, lddtree, pspax, scanelf, scanmacho,symtree)

--- modules/utilities/unix/audit_tools/reversing_tools/manifests/install.pp
+++ modules/utilities/unix/audit_tools/reversing_tools/manifests/install.pp
@@ -1,7 +1,7 @@
 class reversing_tools::install {
 
   Exec { path => ['/bin', '/usr/bin', '/usr/local/bin', '/sbin', '/usr/sbin'] }
-  ensure_packages(['gdb','git', 'ltrace', 'strace'])
+  ensure_packages(['gdb','git', 'ltrace', 'strace', 'pax-utils'])
 
   # Install Radare2
 
