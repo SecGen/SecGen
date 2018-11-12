3e4be586687ada14f37218e2ae9a853cc697af16
ts <thomashaw@localhost.localdomain>
2018-11-12 17:57:59 +0000

    reversing_tools: install cutter

--- modules/utilities/unix/audit_tools/reversing_tools/manifests/install.pp
+++ modules/utilities/unix/audit_tools/reversing_tools/manifests/install.pp
@@ -30,6 +30,33 @@ class reversing_tools::install {
     command => 'make install',
   }
 
+  # Install Cutter (TODO)
+  $cutter_dir = '/opt/Cutter'
+  $cutter_appimage_url = 'https://github.com/radareorg/cutter/releases/download/v1.7.2/Cutter-v1.7.2-x86_64.Linux.AppImage'
+  $cutter_filename = 'Cutter-v1.7.2-x86_64.Linux.AppImage'
+  file { $cutter_dir:
+    ensure => directory,
+  }
+
+  # Download image
+  exec { 'download cutter appimage':
+    command => "/usr/bin/wget -q $cutter_appimage_url -O $cutter_dir/$cutter_filename",
+    cwd => $cutter_dir,
+    require => File[$cutter_dir],
+  }
+
+  exec { 'chmod cutter':
+    command => "/bin/chmod +x $cutter_dir/$cutter_filename",
+    cwd => $cutter_dir,
+    require => Exec['download cutter appimage'],
+  }
+
+  exec { 'install cutter':
+    command => "/usr/bin/install $cutter_dir/$cutter_filename /usr/bin/cutter",
+    cwd => $cutter_dir,
+    require => Exec['download cutter appimage'],
+  }
+
   # Install Detect It Easy from directory (TODO)
 
   # Install angr (TODO)
