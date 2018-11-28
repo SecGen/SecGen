3f392072284fdf4e0536f5b7d57b159462a4b5ce
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2018-11-28 11:29:47 +0000

    lab updates

---
+++ modules/utilities/unix/compromised/dead_image/manifests/install.pp
@@ -0,0 +1,15 @@
+class dead_image::install {
+
+  # $url_path = "http://z.cliffe.schreuders.org/files/6543367533"
+  $url_path = "http://hacktivity.aet.leedsbeckett.ac.uk/files"
+  # This file is just too big and binary to make sense to include in the git repo
+  file {
+      "/root/hda1.img":
+          source => "$url_path/hda1.img"
+  }
+  file {
+      "/root/md5s":
+          source => "$url_path/md5s"
+  }
+
+}
