a53812d065b709412088e77ad4eee711557f7191
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2018-12-07 17:34:00 +0000

    lab updates

--- modules/utilities/unix/compromised/adore_rootkit_static/manifests/install.pp
+++ modules/utilities/unix/compromised/adore_rootkit_static/manifests/install.pp
@@ -5,19 +5,19 @@ class adore_rootkit_static::install {
   # $hidden_ports = join($secgen_parameters['hidden_ports'], "\|")
   # $hidden_strings = join($secgen_parameters['hidden_strings'], "\|")
 
-  file { '/bin/ls':
+  file { '/bin/ls_a':
     source => 'puppet:///modules/adore_rootkit_static/ls',
     mode   => '0755',
     owner => 'root',
     group => 'root',
   }
-  file { '/bin/netstat':
+  file { '/bin/netstat_a':
     source => 'puppet:///modules/adore_rootkit_static/netstat',
     mode   => '0755',
     owner => 'root',
     group => 'root',
   }
-  file { '/bin/ps':
+  file { '/bin/ps_a':
     source => 'puppet:///modules/adore_rootkit_static/ps',
     mode   => '0755',
     owner => 'root',
