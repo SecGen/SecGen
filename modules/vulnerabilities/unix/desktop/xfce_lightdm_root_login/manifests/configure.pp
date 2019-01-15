627ae659c01cbe1005792356da3525b40ca4e57b
ts <thomashaw@localhost.localdomain>
2019-01-15 19:18:47 +0000

    xfce_lightdm_root_login: now works for Stretch server (note: stretch desktop has sddm pre-installed, this module uses lightdm)

--- modules/vulnerabilities/unix/desktop/xfce_lightdm_root_login/manifests/configure.pp
+++ modules/vulnerabilities/unix/desktop/xfce_lightdm_root_login/manifests/configure.pp
@@ -1,5 +1,18 @@
 class xfce_lightdm_root_login::configure {
-  exec { 'lightdm-autologin-root':
-    command => "/bin/sed -i \'/\\[SeatDefaults\\]/a autologin-user=root\' /etc/lightdm/lightdm.conf"
+  case $operatingsystemrelease {
+    /^9.*/: { # do 9.x stretch stuff
+      exec { 'lightdm-autologin-remove-pam':
+        command => "/bin/sed -i \'/auth      required pam_succeed_if.so user != root quiet_success/s/^/#/g\' /etc/pam.d/lightdm-autologin"
+      }
+      file { '/etc/lightdm/lightdm.conf':
+        ensure => present,
+        source => 'puppet:///modules/xfce_lightdm_root_login/stretch_lightdm.conf',
+      }
+    }
+    /^7.*/: { # do 7.x wheezy stuff
+      exec { 'lightdm-autologin-root':
+        command => "/bin/sed -i \'/\\[SeatDefaults\\]/a autologin-user=root\' /etc/lightdm/lightdm.conf"
+      }
+    }
   }
 }
